5e86aec8dec746d83ad3110be41e929d89b0b1cb
Olenka Vargas Lazarte <olenka@ravn.co>
2024-08-07 12:51:28 -0500

    feat: token and active navbar

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -6,10 +6,7 @@ const httpLink = new HttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
-  //   const token = localStorage.getItem('authToken');
-
-  const token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwb3NpdGlvbklkIjoiUmVhY3QgTmVyZGVyeSIsInByb2plY3RJZCI6IjUyZDlkZGNmLTkwOTAtNDU4MS05NmU2LTJkN2U5YTlkNmQyZCIsImZ1bGxOYW1lIjoiT2xlbmthIFZhcmdhcyBMYXphcnRlIiwiZW1haWwiOiJvbGVua2FAcmF2bi5jbyIsImlhdCI6MTcyMjI2OTE2OH0.xVF6w5i-5GrXQixxE83G2pKfrIW8qCqaKB-QfhowBfA';
+  const token = import.meta.env.VITE_AUTH_TOKEN;
 
   return {
     headers: {
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,32 +1,45 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Group } from '@mantine/core';
+
 import logo from '../../assets/images/logo.png';
 import { GroupIcon, ListIcon } from '../ui/Icon';
 import classes from './Navbar.module.css';
 
 const data = [
-  { link: '', label: 'DASHBOARD', icon: <GroupIcon /> },
-  { link: '', label: 'MY TASKS', icon: <ListIcon /> },
+  { link: '/', label: 'DASHBOARD', icon: <GroupIcon /> },
+  { link: '/my-tasks', label: 'MY TASKS', icon: <ListIcon /> },
 ];
 
 function Navbar() {
-  const [active, setActive] = useState('Billing');
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [active, setActive] = useState(location.pathname);
 
-  const links = data.map((item) => (
-    <a
-      className={classes.link}
-      data-active={item.label === active || undefined}
-      href={item.link}
-      key={item.label}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(item.label);
-      }}
-    >
-      {item.icon}
-      <span className={classes.itemLabel}>{item.label}</span>
-    </a>
-  ));
+  const handleClick = (link: string) => {
+    setActive(link);
+    navigate(link);
+  };
+
+  const links = data.map((item) => {
+    const isActive = item.link === active;
+    const iconColor = isActive ? '#DA584B' : 'white';
+    return (
+      <a
+        className={classes.link}
+        data-active={item.link === active || undefined}
+        href={item.link}
+        key={item.label}
+        onClick={(event) => {
+          event.preventDefault();
+          handleClick(item.link);
+        }}
+      >
+        {React.cloneElement(item.icon, { color: iconColor })}
+        <span className={classes.itemLabel}>{item.label}</span>
+      </a>
+    );
+  });
 
   return (
     <nav className={classes.navbar}>
